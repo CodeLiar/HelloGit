f8175757bbf73e700bd7a7ea74b83dfcfdd5bf47
Geass <dmjliar@gmail.com>
2016-10-11 17:17:29 +0800

    init

---
+++ HelloGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloGit
+//
+//  Created by Geass on 10/11/16.
+//  Copyright © 2016 ContinuedStory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGit
+//
+//  Created by Geass on 10/11/16.
+//  Copyright © 2016 ContinuedStory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
